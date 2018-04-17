7c169933ecca5d8d606103c78556bf5131dc5c5e
BatesHighsmith <32876167+BatesHighsmith@users.noreply.github.com>
2018-04-17 11:36:43 -0400

    Jared's Final R/P/S Project, AFTER CORRECTIONS!

--- logic.js
+++ logic.js
@@ -1,54 +1,35 @@
-const options = ['Rock', 'Scissors', 'Paper'];
+const options = ['Paper', 'Scissors', 'Rock'];
 // This function takes in the userChoice and then prints the results to the console.
 function playGame(userChoice) {
     console.log('You chose ' + userChoice + '.');
-if(userChoice == 'Rock'){
-    var a = 2;
+    if (userChoice == 'Rock') {
+        var a = 2;
+    }
+    if (userChoice == 'Scissors') {
+        var a = 1;
+    }
+    if (userChoice == 'Paper') {
+        var a = 0;
+    }
+    const b = Math.floor(Math.random() * 3);
+    if (b == 2) {
+        var c = 'Rock';
+    }
+    if (b == 1) {
+        var c = 'Scissors';
+    }
+    if (b == 0) {
+        var c = 'Paper';
+    }
+    console.log('The computer chose ' + c);
+    if (a == b) {
+        console.log('You tied.');
+    } else if (a == b + 1 || Math.abs(a - b - 1) == 3) {
+        console.log(userChoice + ' beats ' + c + '. You win');
+    } else {
+        console.log(c + ' beats ' + userChoice + '. You lose.');
+    }
 }
-if(userChoice == 'Scissors'){
-    var a = 1;
-}
-if(userChoice == 'Paper'){
-    var a = 0;
-}
-var b = (Math.floor(Math.random() * 3));
-if (b == 2){
-    var c = 'Rock';
-}
-if (b == 1){
-    var c = 'Scissors';
-}
-if (b == 0){
-    var c = 'Paper';
-}
-console.log('The computer chose' + c);
-if(a == b){
-    document.write('You tied.')
-}
-else if(a == b + 1 || Math.abs(a - b - 1) == 3){
-    document.write(userChoice + 'beats' + c + '. You win');
-}
-else{
-    document.write(c + 'beats' + userChoice + '. You lose.');
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports.playGame = playGame;
 module.exports.options = options;
